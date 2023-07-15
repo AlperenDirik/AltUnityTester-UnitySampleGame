674ba39464753f7088222c5cd55ef59eb1cc06e5
Alpeen <alpeen@outlook.com.tr>
2023-07-15 18:03:58 +0300

    First Test added
    When I clicked start button, game will redirect me to the "Main" scene.

---
+++ AltTester/Assets/AltTester/Editor/Tests/AltTrashCatTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using AltTester.AltTesterUnitySDK.Driver;
+
+public class NewAltTest
+{   //Important! If your test file is inside a folder that contains an .asmdef file, please make sure that the assembly definition references NUnit.
+    public AltDriver altDriver;
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        altDriver =new AltDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        altDriver.Stop();
+    }
+
+    [Test]
+    public void TestStartButtonLoadsMainScene()
+    {
+        //Here you can write the test
+        altDriver.LoadScene("Start");
+        altDriver.FindObject(By.TEXT, "StartButton").Click();
+        altDriver.WaitForCurrentSceneToBe("Main");
+    }
+
+}
